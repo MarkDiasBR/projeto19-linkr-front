64da85099f8b5f16ae9c817b413eff7daa103f66
Mateus Barcelos <mateuspit@hotmail.com>
2023-06-05 17:50:31 -0300

    feat: searchbar working

--- src/components/Header/SearchBar.jsx
+++ src/components/Header/SearchBar.jsx
@@ -1,14 +1,14 @@
 import { useRef, useState, useEffect } from "react";
-//import styled from "styled-components";
+import styled from "styled-components";
 import axios from "axios";
 import { AiOutlineSearch } from "react-icons/ai";
 import { SearchContainer, SearchIcon, SearchInput, DropdownContainer } from "./SearchBarStyle";
 
 export default function SearchBar() {
     const [searchNick, setSearchNick] = useState("");
-    const dropdownRef = useRef();
     const debounceTimeoutRef = useRef(null);
-    //const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+    const [searchResult, setSearchResult] = useState([]);
     //const test1DropDown = [{ urlp: "https://static.todamateria.com.br/upload/mi/ch/michelangelo-og.jpg", username: "JOAZINHO" }, { urlp: "https://aventurasnahistoria.uol.com.br/media/_versions/legacy/2017/04/15/davinciandndn", username: "JOAZÃO" }, { urlp: "https://static.todamateria.com.br/upload/do/na/donatello-cke.jpg", username: "JOAO" }];
     //const test2DropDown = [{ urlp: "https://static.todamateria.com.br/upload/mi/ch/michelangelo-og.jpg", username: "JOAZINHO" }, { urlp: "https://aventurasnahistoria.uol.com.br/media/_versions/legacy/2017/04/15/davinciandndn", username: "JOAZÃO" }]
     //const test3DropDown = [{ urlp: "https://aventurasnahistoria.uol.com.br/media/_versions/legacy/2017/04/15/davinciandndn", username: "JOAZÃO" }]
@@ -21,6 +21,7 @@ export default function SearchBar() {
     //}
 
     useEffect(() => {
+        let actualSearchResult = ([]);
         const config = {
             headers: {
                 Authorization: "Bearer f5e16715-b3b3-43eb-a575-c612106a1085",
@@ -39,13 +40,20 @@ export default function SearchBar() {
                     searchThis: searchNick
 
                 }, config)
-                //axios.get('http://localhost:5000/search', {
+                    //axios.get('http://localhost:5000/search', {
 
-                //    searchThis: searchNick
+                    //    searchThis: searchNick
 
-                //}, config)
+                    //}, config)
                     .then((res) => {
                         console.log(res.data);
+                        actualSearchResult = res.data;
+                        setSearchResult(actualSearchResult);
+                        console.log(res.data.length);
+                        console.log(!!res.data.length);
+                        if (res.data.length) {
+                            setIsOpen(true);
+                        }
                     })
                     .catch((error) => {
                         console.error(error);
@@ -59,9 +67,12 @@ export default function SearchBar() {
                 clearTimeout(debounceTimeoutRef.current);
             };
         }
+        else {
+            setIsOpen(false);
+        }
     }, [searchNick]);
     return (
-        <DropdownContainer ref={dropdownRef}>
+        <DropdownContainer>
             <SearchContainer>
                 <SearchInput
                     type="text"
@@ -72,7 +83,61 @@ export default function SearchBar() {
                     onChange={(e) => setSearchNick(e.target.value)}
                 />
                 <SearchIcon><AiOutlineSearch style={{ height: "30px", width: "30px", color: "#C6C6C6" }} /></SearchIcon>
+                <DropdownOptions displayDropdown={isOpen}>
+                    {searchResult.map((asr, index) => {
+                        console.log("isOpen", isOpen);
+                        return (
+                            <SearchedUserContainer key={index}>
+                                <SearchedUserIcon src={asr.urlp} alt={asr.username} />
+                                <SearchedUserUsername>{asr.username}</SearchedUserUsername>
+                            </SearchedUserContainer>
+                        );
+                    })}
+                </DropdownOptions>
             </SearchContainer>
         </DropdownContainer>
     );
 }
+
+const SearchedUserContainer = styled.div`
+    display: flex;
+    background: #E7E7E7;
+    margin-top: 16px;
+    align-items: center;
+    cursor: pointer;
+`;
+
+const SearchedUserUsername = styled.h1`
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 19px;
+    line-height: 23px;
+    color: #515151;
+    margin-left: 15px;
+`;
+
+const SearchedUserIcon = styled.img`
+    width: 39px;
+    height: 39px;
+    border-radius: 50%;
+`;
+
+const DropdownOptions = styled.div`
+  display: ${(props) => (props.displayDropdown ? 'flex' : 'none')};
+  flex-direction: column;
+  padding-left: 17px;
+  padding-bottom: 23px;  
+  margin-top: -20px;
+  position: absolute;
+  top: 100%;
+  left: 0;
+  width: 100%;
+  list-style-type: none;
+  /*padding: 0;*/
+  /*margin: 0;*/
+  border: 1px solid #ccc;
+  border-top: none;
+  border-radius: 0 0 4px 4px;
+  background: #E7E7E7;
+`;
